fee9b0e0feae88a45048b4850179a3c68d499157
nyame <nyamebismark12.nb@gmail.com>
2022-11-19 14:09:57 +0000

    Changes the labels name and cleaning some of the layout files

--- app/src/main/java/com/bismark/currency/HistoricalDataFragment.kt
+++ app/src/main/java/com/bismark/currency/HistoricalDataFragment.kt
@@ -21,8 +21,5 @@ class HistoricalDataFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 }
