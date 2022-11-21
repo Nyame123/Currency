6b045e9ed3ae7c0c879de93ea9ca52ae140c6226
nyame <nyamebismark12.nb@gmail.com>
2022-11-21 19:02:55 +0000

    Cleaned the code of any unused functions

--- app/src/main/java/com/bismark/currency/core/Either.kt
+++ app/src/main/java/com/bismark/currency/core/Either.kt
@@ -13,34 +13,3 @@ sealed class Either<out L, out R> {
     fun <L> left(a: L) = Left(a)
     fun <R> right(b: R) = Right(b)
 }
-
-// Credits to Alex Hart -> https://proandroiddev.com/kotlins-nothing-type-946de7d464fb
-// Composes 2 functions
-fun <A, B, C> ((A) -> B).c(f: (B) -> C): (A) -> C = {
-    f(this(it))
-}
-
-/**
- * Binds the given function across [Either.Right].
- *
- * @param f The function to bind across [Either.Right].
- */
-inline fun <A, B, C> Either<A, B>.flatMap(f: (B) -> Either<A, C>): Either<A, C> =
-    when (this) {
-        is Either.Right -> f(this.b)
-        is Either.Left -> this
-    }
-
-fun <T, L, R> Either<L, R>.map(fn: (R) -> (T)): Either<L, T> = this.flatMap(fn.c(::right))
-
-/**
- * Perform the function lambda when result is successful
- */
-suspend fun <L, R> Either<L, R>.onSuccessSuspended(fn: suspend (success: R) -> Unit): Either<L, R> =
-    this.apply { if (this is Either.Right) fn(b) }
-
-/**
- * Perform the function lambda when the result is failed
- */
-suspend fun <L, R> Either<L, R>.onFailureSuspended(fn: suspend (failure: L) -> Unit): Either<L, R> =
-    this.apply { if (this is Either.Left) fn(a) }
--- app/src/main/java/com/bismark/currency/extensions/StringUtil.kt
+++ app/src/main/java/com/bismark/currency/extensions/StringUtil.kt
@@ -1,21 +1,6 @@
 package com.bismark.currency.extensions
 
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-
 /**
  * Returns empty String with no characters
  **/
 fun String.Companion.empty(): String = ""
-
-/**
- * Return the receiver or the empty String if the receiver is `null`
- **/
-fun String?.toSafeString(): String {
-    if (this == null) return String.empty()
-    return toString()
-}
-
-fun String.toDate(): Date? = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(this)
-
